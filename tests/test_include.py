aef2d58b6932e1ee342a88344c3e58a4c7d27f96
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2018-02-05 10:03:33 +0000

    Unittest for circular check

--- tests/test_include.py
+++ tests/test_include.py
@@ -35,3 +35,34 @@ b: !include {}
         '''.format(f.name))
 
         self.assertEqual(data, {'a': 1, 'b': 2})
+
+    def test_circular_self(self):
+        f = tempfile.NamedTemporaryFile(suffix='.yaml')
+
+        f.write('''
+a: !include {}
+        '''.format(f.name).encode())
+        f.flush()
+        f.seek(0)
+
+        with self.assertRaises(ValueError):
+            yamlenv.load(f)
+
+    def test_circular(self):
+        f1 = tempfile.NamedTemporaryFile(suffix='.yaml')
+        f2 = tempfile.NamedTemporaryFile(suffix='.yaml')
+
+        f1.write('''
+a: !include {}
+        '''.format(f2.name).encode())
+        f1.flush()
+        f1.seek(0)
+
+        f2.write('''
+a: !include {}
+        '''.format(f1.name).encode())
+        f2.flush()
+        f2.seek(0)
+
+        with self.assertRaises(ValueError):
+            yamlenv.load(f1)
