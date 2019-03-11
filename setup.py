e90f561985d015c7c05664317930fb3255591db8
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2019-03-11 09:51:03 +0000

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as readme:
 
 setup(
     name='yamlenv',
-    version='0.7.0',
+    version='0.7.1',
     author="Lorenzo Bolla",
     author_email="lbolla@gmail.com",
     description="Interpolate Yaml files with env vars",
